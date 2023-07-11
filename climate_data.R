663fc025691c553ced85673e969176af01860662
De La Pascua <danielle_delapascua@fws.gov>
2023-07-11 14:22:27 -0700

    add additional data & fix climate variables

--- climate_data.R
+++ climate_data.R
@@ -35,6 +35,12 @@ agg_tbl
 df1 <- agg_tbl %>% as.data.frame()
 df1
 
+# converting to "last november"
+min_temp <- df1$Nov_min
+min_temp <- head(min_temp, -1)
+years <- c(1986:2022)
+last_nov_min_temp <- data.frame(years, min_temp)
+
 ### November maximum temperature
 
 # Group by mean using dplyr
@@ -43,13 +49,19 @@ agg_tbl <- dat %>%
   filter(Month == 11) %>% #filter only the month we want
   mutate(mass_norm = Air.max / mean(Air.max, na.rm = TRUE)) %>% #calculate means at 'min'
   group_by(Year) %>% 
-  summarise(Nov_max=mean(Air.max), #name the new means column
+  summarise(max_temp=mean(Air.max), #name the new means column
             .groups = 'drop')
 agg_tbl
 
 # Convert tibble to df
 df2 <- agg_tbl %>% as.data.frame()
 df2
+
+# converting to "last november"
+max_temp <- df2$max_temp
+max_temp <- head(max_temp, -1)
+years <- c(1986:2022)
+last_nov_max_temp <- data.frame(years, min_temp)
 
 ### August maximum temperature
 
@@ -65,7 +77,7 @@ agg_tbl
 
 # Convert tibble to df
 df3 <- agg_tbl %>% as.data.frame()
-df3
+august_max_temp <- df3
 
 ### Dec min temperature
 
@@ -83,6 +95,12 @@ agg_tbl
 df4 <- agg_tbl %>% as.data.frame()
 df4
 
+# converting to "last december"
+min_temp <- df4$Dec_min
+min_temp <- head(min_temp, -1)
+years <- c(1986:2022)
+last_dec_min_temp <- data.frame(years, min_temp)
+
 ### January min temperature
 
 # Group by mean using dplyr
@@ -97,7 +115,7 @@ agg_tbl
 
 # Convert tibble to df
 df5 <- agg_tbl %>% as.data.frame()
-df5
+january_min_temp <- df5
 
 ### September max temperature
 
@@ -115,6 +133,12 @@ agg_tbl
 df6 <- agg_tbl %>% as.data.frame()
 df6
 
+# converting to "last september"
+max_temp <- df6$Sept_max
+max_temp <- head(max_temp, -1)
+years <- c(1986:2022)
+last_nov_min_temp <- data.frame(years, max_temp)
+
 ### July max temperature
 
 # Group by mean using dplyr
@@ -129,7 +153,7 @@ agg_tbl
 
 # Convert tibble to df
 df7 <- agg_tbl %>% as.data.frame()
-df7
+july_max_temp <- df7
 
 # Fall precipitation
 
@@ -183,8 +207,14 @@ precip_nov
 
 # Add september, october, and november precipitation values
 fall_precip<- precip_sept$Fall_precip + precip_oct$Fall_precip + precip_nov$Fall_precip
-years <- c(1986:2022)
+years <- c(1985:2022)
 precip_fall <- data.frame(years, fall_precip)
+
+#make 'last fall' variable
+precip_fall
+values <- precip_fall$fall_precip
+years <- c(1986:2023)
+last_fall_precip <- data.frame(years, values)  
 
 # Winter precipitation
 
@@ -234,13 +264,23 @@ agg_tbl
 precip_feb <- agg_tbl %>% as.data.frame()
 precip_feb 
 
-## i need to fix the variables, need to get december from the year before to add to winter precip
+## fixing december to be "last december"
 
 precip_dec
 
 values <- precip_dec$winter_precip
+years <- c(1986:2023)
 
+precip_last_dec <- data.frame(years, values)
+
+#add all values
+winter_precip <- c(precip_last_dec$values + precip_jan$winter_precip + precip_feb$winter_precip)
+winter_precip <- head(winter_precip, -1)
+years <- c(1986:2022)
+precip_winter <- data.frame(years, winter_precip)
 
 #Winter temperature standard deviation
+
+
 
 #winter precipitation standard variation
